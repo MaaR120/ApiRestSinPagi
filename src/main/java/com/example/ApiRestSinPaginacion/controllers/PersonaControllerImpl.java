ce04d38603ef384912a1b68b00961363a5663688
MaaR120 <94024138+MaaR120@users.noreply.github.com>
2023-10-04 21:43:28 -0300

    Update PersonaControllerImpl.java

--- src/main/java/com/example/ApiRestSinPaginacion/controllers/PersonaControllerImpl.java
+++ src/main/java/com/example/ApiRestSinPaginacion/controllers/PersonaControllerImpl.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "apirestsinpaginacion.com/persona")
-public class PersonaControllerImpl extends BaseControllerImpl<Persona, PersonaServiceImpl> implements BaseController<Persona,Long> {
+@RequestMapping(path = "api/v1/persona")
+public class PersonaControllerImpl extends BaseControllerImpl<Persona, PersonaServiceImpl>{
 }
